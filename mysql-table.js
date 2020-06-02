099730394ba24d0c7a8305e5c20be46877e28805
Ciarra Murphy <mciarra@ymail.com>
2020-06-02 16:50:39 -0500

    added mysql file

--- mysql-table.js
+++ mysql-table.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var mysql = require('./dbcon.js');
+
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
-var bodyParser = require('body-parser');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -10,3 +11,20 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 60790);
 
+
+
+app.use(function(req,res){
+    res.status(404);
+    res.render('404');
+  });
+  
+  app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('plain/text');
+    res.status(500);
+    res.render('500');
+  });
+  
+  app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  });
