78216dea90bfa13e68398194b57408c6860a691f
Ciarra Murphy <mciarra@ymail.com>
2020-06-02 20:18:16 -0500

    added static file

--- mysql-table.js
+++ mysql-table.js
@@ -4,27 +4,65 @@ var mysql = require('./dbcon.js');
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 60790);
 
 
+//app.get('/', function(req,res,next){
+    //var selectTable = {};
+    //mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
+        //if(err){
+            //next(err);
+            //return;
+        //}
+        //selectTable.results = JSON.stringify(rows);
+        //res.render('home', selectTable);
+    //});
+//});
+
+app.get('/', function(req,res,next){
+    var insetData = {};
+    mysql.pool.query("INSERT INTO workouts(`name`, `reps`, `weight`, `date`, `lbs`) VALUES(?, ?, ?, ?, ?)", [req.query.response], function(err, result){
+        if(err){
+            next(err);
+            return;
+        }
+        console.log(context.results);
+        res.render('home',insertData);
+    })
+})
+
+// added the code from the assignment page for testing
+app.get('/reset-table',function(req,res,next){
+    var context = {};
+    mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ 
+      var createString = "CREATE TABLE workouts("+
+      "id INT PRIMARY KEY AUTO_INCREMENT,"+
+      "name VARCHAR(255) NOT NULL,"+
+      "reps INT,"+
+      "weight INT,"+
+      "date DATE,"+
+      "lbs BOOLEAN)";
+      mysql.pool.query(createString, function(err){
+          context.results = "Table reset";
+          res.render('home',context);
+      })
+    });
+});
 
 app.use(function(req,res){
     res.status(404);
     res.render('404');
-  });
+});
   
-  app.use(function(err, req, res, next){
+app.use(function(err, req, res, next){
     console.error(err.stack);
     res.type('plain/text');
     res.status(500);
     res.render('500');
-  });
+});
   
-  app.listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
-  });
+});
